ca446695290a457d4e180e8f24bdd9eaa1df98eb
z.h.e <alnasser_zahraa@hotmail.com>
2021-07-05 14:05:45 +0300

    = adding tailwind to the project1

---
+++ resources/views/r.blade.php
@@ -0,0 +1,5 @@
+
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<p CLASS="text-green-300"  >we</p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        {{--<meta charset="UTF-8" />--}}
+        {{--<meta name="viewport" content="width=device-width, initial-scale=1.0" />--}}
+        {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+        {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+        <title class="text-gray-200">Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        {{--<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
 
         <!-- Styles -->
         <style>
