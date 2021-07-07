53fa5f799fa8a971c94c29371a56cd030c84b1fa
z.h.e <alnasser_zahraa@hotmail.com>
2021-07-07 20:09:20 +0300

    model item

---
+++ app/Models/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+use App\Traits\Base;
+use App\Traits\Ctmn;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+   use Base, Ctmn;
+
+    protected $guarded = ['id'];
+    protected $appends = ['cover'];
+
+//    protected $with = ['media', 'category', 'tags'];
+
+}
