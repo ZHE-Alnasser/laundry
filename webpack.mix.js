64fa96564d026e715c43f3a9c9615196647eb42d
z.h.e <alnasser_zahraa@hotmail.com>
2021-07-14 21:22:50 +0300

    update tailwind.config.js & webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
+if (!mix.inProduction()) {
+    // mix.browserSync({ proxy: "http://pos.test" });
+
+
+    mix.browserSync({
+        proxy: 'http://laundry.test',
+        // injectChanges: false,
+       open: false,
+    });
+
+}
+
 mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
