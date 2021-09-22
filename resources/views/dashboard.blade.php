97fb251ee8d50ce83e67402593fe573e00d02e81
z.h.e <alnasser_zahraa@hotmail.com>
2021-09-23 00:45:58 +0300

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,131 @@
 
     <x-layouts.admin>
-        <div class="mt-16 text-right">
-            <x-index-card>
-                <h1 class=" mx-10 text-center text-3xl">{{__('Welcome')}}</h1>
-                <h2 class=" mt-10 text-center text-2xl ">{{$firstName}} {{$lastName}}</h2>
-            </x-index-card>
+        <div class="mt-5 ">
+            {{--<x-index-card>--}}
+                {{--<h1 class=" mx-10 text-center text-3xl">{{__('Welcome')}}</h1>--}}
+                {{--<h2 class=" mt-10 text-center text-2xl ">{{$firstName}} {{$lastName}}</h2>--}}
+            <div class="grid grid-cols-4 grid-flow-row gap-2">
+                <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400">
+                    <a href="{{url('users')}}">
+                        <img src="{{asset('img/svg/users.svg')}}"/>
+                    <span class="text-center text-3xl text-color">{{__('Users')}}</span>
+                    </a>
+                    </div>
+
+
+        <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('orders')}}">
+
+                <img src="{{asset('img/svg/order.svg')}}"/>
+
+                    <span class="text-center text-3xl text-color">@lang('Orders')</span>
+
+             </a></div>
+    <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('items')}}">
+            <div class="info-box bg-green">
+                <img src="{{asset('img/svg/item.svg')}}"/>
+                <span class="text-center text-3xl text-color">@lang('Items')</span>
+
+            </div></a></div>
+    <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('services/manage')}}">
+            <div class="info-box bg-green">
+                <img src="{{asset('img/svg/service.svg')}}"/>
+                <span class="text-center text-3xl text-color">@lang('Services')</span>
+
+            </div></a></div>
+    <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('cities')}}">
+            <div class="info-box bg-green">
+                <img src="{{asset('img/svg/city.svg')}}"/>
+                <span class="text-center text-3xl text-color">@lang('Cities')</span>
+
+            </div></a></div>
+
+    <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('districts')}}">
+            <div class="info-box bg-green">
+                <img src="{{asset('img/svg/map.svg')}}"/>
+                <span class="text-center text-3xl text-color">@lang('Districts')</span>
+
+            </div></a></div>
+
+                <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('roles')}}">
+            <div class="info-box bg-green">
+                <img src="{{asset('img/svg/role.svg')}}"/>
+
+                <span class="text-center text-color">@lang('Roles')</span>
+
+            </div></a></div>
+
+                <div class=" bg-teal-400 card rounded-2xl border-2 border-gray-400 ">  <a href="{{url('settings')}}">
+            <div class="info-box bg-green">
+                <img src="{{asset('img/svg/settings.svg')}}"/>
+                <span class="text-center text-3xl text-color">@lang('Settings')</span>
+
+            </div></a></div>
+</div>
+    {{--<div class="grid cols-4">--}}
+                    {{--@if(auth()->user()->role->name == 'admin')--}}
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<a href="{{url('users')}}">--}}
+                                {{--<div class="info-box bg-green">--}}
+                                    {{--<span class="info-box-icon"><i class="glyphicon glyphicon-th"></i></span>--}}
+                                    {{--<div class="info-box-content text-center center-block"><br>--}}
+                                        {{--<span class="info-box-text">{{__('Manage Users')}}</span>--}}
+                                    {{--</div>--}}
+                                {{--</div></a>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<a href="{{url('groups')}}">--}}
+                                {{--<div class="info-box bg-green">--}}
+                                    {{--<span class="info-box-icon"><i class="glyphicon glyphicon-th"></i></span>--}}
+                                    {{--<div class="info-box-content text-center center-block"><br>--}}
+                                        {{--<span class="info-box-text">@lang('site.groups')</span>--}}
+                                    {{--</div>--}}
+                                {{--</div></a>--}}
+                        {{--</div>--}}
+
+
+
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<a href="{{url('services')}}">--}}
+                                {{--<div class="info-box bg-green">--}}
+                                    {{--<span class="info-box-icon"><i class="glyphicon glyphicon-th"></i></span>--}}
+                                    {{--<div class="info-box-content text-center center-block"><br>--}}
+                                        {{--<span class="info-box-text">@lang('Mange Services')</span>--}}
+                                    {{--</div>--}}
+                                {{--</div></a>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<a href="{{url('cities')}}">--}}
+                                {{--<div class="info-box bg-green">--}}
+                                    {{--<span class="info-box-icon"><i class="glyphicon glyphicon-th"></i></span>--}}
+                                    {{--<div class="info-box-content text-center center-block"><br>--}}
+                                        {{--<span class="info-box-text">@lang('Manage Cities')</span>--}}
+                                    {{--</div>--}}
+                                {{--</div></a>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<a href="{{url('districts')}}">--}}
+                                {{--<div class="info-box bg-green">--}}
+                                    {{--<span class="info-box-icon"><i class="glyphicon glyphicon-th"></i></span>--}}
+                                    {{--<div class="info-box-content text-center center-block"><br>--}}
+                                        {{--<span class="info-box-text">@lang('Manage Districts')</span>--}}
+                                    {{--</div>--}}
+                                {{--</div></a>--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
+                    {{--<div class="col-sm-6 col-xs-12">--}}
+                        {{--<a href="{{url('orders')}}">--}}
+                            {{--<div class="info-box bg-green">--}}
+                                {{--<span class="info-box-icon"><i class="glyphicon glyphicon-th"></i></span>--}}
+                                {{--<div class="info-box-content text-center center-block"><br>--}}
+                                    {{--<span class="info-box-text">@lang('Manage Orders')</span>--}}
+                                {{--</div>--}}
+                            {{--</div></a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</x-index-card>--}}
         </div>
     </x-layouts.admin>
 
