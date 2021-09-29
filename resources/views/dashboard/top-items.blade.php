edefbcb14b66d5be776e2df4a633faad0a29488c
z.h.e <alnasser_zahraa@hotmail.com>
2021-09-29 20:47:07 +0300

    wip

--- resources/views/dashboard/top-items.blade.php
+++ resources/views/dashboard/top-items.blade.php
@@ -5,7 +5,7 @@
             <h2 class="text-gray-800 font-bold">{{__('Most Requested Services')}}</h2>
             <hr class="my-5">
             <div class="row">
-                <table class=" table-bordered w-full using-font col-11 mx-2">
+                <table class=" text-gray-600 w-full using-font col-11 mx-2">
                     <thead class="bg-gray-100">
                     <th class="py-3">@lang('Service Name')</th>
                     <th class="py-3">@lang('Number of Orders')</th>
