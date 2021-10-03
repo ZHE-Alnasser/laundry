23e1e17a615a857200cf6d7ab31c9d307f9d6252
z.h.e <alnasser_zahraa@hotmail.com>
2021-10-03 12:54:34 +0300

    uiform the edge

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div  class="bg-white overflow-hidden p-5 no-underline hover:no-underline shadow-lg">
+<div  class="bg-white overflow-hidden p-5 no-underline rounded-lg hover:no-underline shadow-lg">
 
     {{$slot}}
 </div>
--- resources/views/components/layouts/_sidebar.blade.php
+++ resources/views/components/layouts/_sidebar.blade.php
@@ -40,13 +40,13 @@
 {{--<x-layouts.base>--}}
     <script src="https://unpkg.com/feather-icons"></script>
 
-    <div id="sidebar" class="bg-gray-200">
+    <div id="sidebar" class="">
     <div class="md:flex flex-col md:flex-row md:min-h-screen w-full">
 {{--<div class="md:flex flex-col md:flex-row md:min-h-screen w-full">--}}
     {{--<div class="flex flex-col w-full md:w-64 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0">--}}
         {{--<nav class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">--}}
 
-        <div class="bg-white pb-3 ">
+        <div class="bg-white pb-3  rounded-lg">
 
                 <button class="flex-row text-gray-500 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent
         rounded bg-transparent block md:hidden outline-none focus:outline-none mr-2"
