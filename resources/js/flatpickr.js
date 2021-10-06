b4879824317bea2ac1d1f4f6810a1bd62aed01e1
z.h.e <alnasser_zahraa@hotmail.com>
2021-10-06 09:30:17 +0300

    clean up

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -1,17 +1,4 @@
-// import flatpckr from "flatpickr";
-// flatpckr('.flatpickr.js-flatpickr-dateTime',{
-//     dateFormat: 'Y-m-d'
-// });
-//
-// let options = {
-//     mode: "range",
-//     dateFormat: "Y-m-d"
-// };
-//
-//
-// flatpckr('.date-range',options);
 
 import flatpickr from "flatpickr";
 
 window.flatpickr = flatpickr;
-//
